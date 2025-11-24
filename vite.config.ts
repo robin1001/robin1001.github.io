5e38980c01b9e04c0901b3e0136eb0ead669bb96
Binbin Zhang <binbzha@qq.com>
2025-11-24 23:24:18 +0800

    auto deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // 固定入口 JS 文件名
+        entryFileNames: 'assets/[name].js',
+        // 固定代码分割块文件名
+        chunkFileNames: 'assets/[name].js',
+        // 固定静态资源文件名（包括 CSS）
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/[name].css'
+          }
+          return 'assets/[name].[ext]'
+        },
+      },
+    },
+  },
 })
-
